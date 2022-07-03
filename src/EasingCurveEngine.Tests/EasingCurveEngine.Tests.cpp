edea29dc6866abccf1df1d7ddf58b8981e009c78
Harrison Brown <8903393+HarrisonBrown@users.noreply.github.com>
2022-07-03 16:08:48 +0100

    testagainst bad parameter values

--- src/EasingCurveEngine.Tests/EasingCurveEngine.Tests.cpp
+++ src/EasingCurveEngine.Tests/EasingCurveEngine.Tests.cpp
@@ -46,6 +46,24 @@ namespace EasingCurveEngineTests
 			Assert::AreEqual(expectedCode, actualCode);
 		}
 
+		TEST_METHOD(BadValuesString)
+		{
+			std::string command = "Linear, x_t0 = bad, x_tmax = 200, duration = 1";
+			int expectedCode = (int)ValidationCode::BAD_VALUE;
+			int actualCode = (int)ValidateCommand(command);
+
+			Assert::AreEqual(expectedCode, actualCode);
+		}
+
+		TEST_METHOD(BadValuesFloat)
+		{
+			std::string command = "Linear, x_t0 = 5.5, x_tmax = 200, duration = 1";
+			int expectedCode = (int)ValidationCode::BAD_VALUE;
+			int actualCode = (int)ValidateCommand(command);
+
+			Assert::AreEqual(expectedCode, actualCode);
+		}
+
 		TEST_METHOD(ValidQuery)
 		{
 			std::string command = "1.0";
